af23d9faf082f643f21e039247b48a715ae02770
tujiaw <tujiaw@163.com>
2019-07-03 14:01:23 +0800

    add

--- index.js
+++ index.js
@@ -26,11 +26,11 @@ const publicDir = path.join(__dirname, 'public')
 app.use(require('koa-static')(publicDir))
 app.use(controller.startRoute)
 app.use(router.routes()).use(router.allowedMethods())
-router.post('/delete', controller.delete)
 router.post('/upload', controller.upload)
 router.post('/new', controller.new)
-router.post('/move', controller.move)
 router.post('/archive', controller.archive)
+router.post('/delete', controller.delete)
+router.post('/move', controller.move)
 app.use(controller.main)
 
 const port = 5000;
