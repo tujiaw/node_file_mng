e0e9b98c0ad76dbc13e77ae4a0e5c0df27527f88
tujiaw <tujiaw@163.com>
2019-07-03 13:47:38 +0800

    排序优化

--- common/test.js
+++ common/test.js
@@ -1,3 +1,31 @@
-console.log(new Date().toISOString())
+const fs = require('fs')
+const path = require('path')
 
-console.log('files-' + new Date().toISOString().replace(/:/g, ''))
+const DIR = path.normalize(__dirname)
+
+function fetchFiles(dir, fn) {
+    const ls = fs.readdirSync(dir)
+    if (!ls) return
+    ls.forEach(name => {
+        const subfile = path.join(dir, name)
+        const st = fs.lstatSync(subfile)
+        if (st && st.isDirectory()) {
+            fetchFiles(subfile, fn)
+        } else if (st && st.isFile()) {
+            fn(subfile)
+        }
+    })
+}
+
+const SEARCH_TEXT = '-std=c++03'
+const REPLACE_TEXT = '-std=c++98'
+const MATCH_LIST = ['Makefile']
+fetchFiles(DIR, filepath => {
+    if (!MATCH_LIST.includes(path.basename(filepath))) return
+    const content = fs.readFileSync(filepath, 'utf-8')
+    if (content && content.indexOf(SEARCH_TEXT) >= 0) {
+        const result = content.replace(SEARCH_TEXT, REPLACE_TEXT)
+        console.log(result)
+        fs.writeFileSync(filepath, result)
+    }
+})
